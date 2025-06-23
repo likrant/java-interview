d7c7be766f4b0207273117680b9fc74274726de1
Iurii Luzan <40656664+likrant@users.noreply.github.com>
2025-06-23 19:01:43 +0300

    Add Lombok and resource-based parser

---
+++ interview-web/src/test/java/com/li/javainterview/InterviewWebApplicationTests.java
@@ -0,0 +1,12 @@
+package com.li.javainterview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
